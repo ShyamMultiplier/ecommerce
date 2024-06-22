a78e1043d78ed43cc935006c29f2b72785c669dc
Santhosh Kumar <sangamalla.santhoshkumar2002@gmail.com>
2024-06-23 01:56:49 +0530

    Updated Product Endpoints and Pagination

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const getProducts = require("../controllers/productController")
+const {getProducts, getProductById, getBestsellers, adminGetProducts} = require("../controllers/productController")
 
+router.get("/category/:categoryName/search/:searchQuery", getProducts)
+router.get("/category/:categoryName", getProducts)
+router.get("/search/:searchQuery", getProducts)
 router.get("/", getProducts)
+router.get("/bestsellers", getBestsellers)
+router.get("/get-one/:id", getProductById)
+
+// admin routes:
+router.get("/admin", adminGetProducts)
 
 module.exports = router
